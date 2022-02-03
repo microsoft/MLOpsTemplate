aa6d870eec0be987e01699a8499b1e0b4ee122dd
james-tn <james.nguyen@microsoft.com>
2022-02-03 14:52:11 -0800

    enhance example

--- code/example/train/hello_iris.py
+++ code/example/train/hello_iris.py
@@ -1,20 +1,68 @@
+# imports
 import os
+import mlflow
 import argparse
+
 import pandas as pd
+
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 
 # define functions
 def main(args):
+    # enable auto logging
+    mlflow.autolog()
+
+    # setup parameters
+    params = {
+        "C": args.C,
+        "kernel": args.kernel,
+        "degree": args.degree,
+        "gamma": args.gamma,
+        "coef0": args.coef0,
+        "shrinking": args.shrinking,
+        "probability": args.probability,
+        "tol": args.tol,
+        "cache_size": args.cache_size,
+        "class_weight": args.class_weight,
+        "verbose": args.verbose,
+        "max_iter": args.max_iter,
+        "decision_function_shape": args.decision_function_shape,
+        "break_ties": args.break_ties,
+        "random_state": args.random_state,
+    }
+
     # read in data
     df = pd.read_csv(args.iris_csv)
 
-    # print first 5 lines
-    print(df.head())
+    # process data
+    X_train, X_test, y_train, y_test = process_data(df, args.random_state)
 
-    # ensure outputs directory exists
-    os.makedirs("outputs", exist_ok=True)
+    # train model
+    model = train_model(params, X_train, X_test, y_train, y_test)
 
-    # save data to outputs
-    df.to_csv("outputs/iris.csv", index=False)
+
+def process_data(df, random_state):
+    # split dataframe into X and y
+    X = df.drop(["species"], axis=1)
+    y = df["species"]
+
+    # train/test split
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=random_state
+    )
+
+    # return split data
+    return X_train, X_test, y_train, y_test
+
+
+def train_model(params, X_train, X_test, y_train, y_test):
+    # train model
+    model = SVC(**params)
+    model = model.fit(X_train, y_train)
+
+    # return model
+    return model
 
 
 def parse_args():
@@ -23,6 +71,21 @@ def parse_args():
 
     # add arguments
     parser.add_argument("--iris-csv", type=str)
+    parser.add_argument("--C", type=float, default=1.0)
+    parser.add_argument("--kernel", type=str, default="rbf")
+    parser.add_argument("--degree", type=int, default=3)
+    parser.add_argument("--gamma", type=str, default="scale")
+    parser.add_argument("--coef0", type=float, default=0)
+    parser.add_argument("--shrinking", type=bool, default=False)
+    parser.add_argument("--probability", type=bool, default=False)
+    parser.add_argument("--tol", type=float, default=1e-3)
+    parser.add_argument("--cache_size", type=float, default=1024)
+    parser.add_argument("--class_weight", type=dict, default=None)
+    parser.add_argument("--verbose", type=bool, default=False)
+    parser.add_argument("--max_iter", type=int, default=-1)
+    parser.add_argument("--decision_function_shape", type=str, default="ovr")
+    parser.add_argument("--break_ties", type=bool, default=False)
+    parser.add_argument("--random_state", type=int, default=42)
 
     # parse args
     args = parser.parse_args()
