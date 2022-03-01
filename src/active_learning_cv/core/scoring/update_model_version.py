c86fcc008a4d0453fb6994b80bf2542dbca69e11
james-tn <james.nguyen@microsoft.com>
2022-02-28 21:29:08 -0800

    update SP authentication

--- src/active_learning_cv/core/scoring/update_model_version.py
+++ src/active_learning_cv/core/scoring/update_model_version.py
@@ -1,10 +1,16 @@
 import argparse
 from strictyaml import load
 from azureml.core import Workspace, Model
-
+from azureml.core.authentication import ServicePrincipalAuthentication
+import os
 def main(args):
     # read in data
-    ws = Workspace.get(name="ws01ent",subscription_id="0e9bace8-7a81-4922-83b5-d995ff706507", resource_group="azureml")  
+    secret = os.environ.get("SP_SECRET")
+    client_id = os.environ.get("SP_ID")
+    tenant_id = "72f988bf-86f1-41af-91ab-2d7cd011db47"
+
+    sp = ServicePrincipalAuthentication(tenent_id=tenant_id, service_principal_id=client_id,service_principal_password=secret)
+    ws = Workspace.from_config(auth=sp)  
     current_version= Model(ws,args.model_name).version
     with open(args.job_file, 'r') as yml_file:
         yml_content = yml_file.read()
@@ -21,6 +27,8 @@ def parse_args():
     # add arguments
     parser.add_argument("--job_file", type=str)
     parser.add_argument("--model_name", type=str)
+    parser.add_argument("--client_id", type=str)
+
     # parse args
     args = parser.parse_args()
 
