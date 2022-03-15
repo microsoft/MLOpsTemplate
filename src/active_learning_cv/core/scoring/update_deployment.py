f84e48437b8cb2e6a43a9e343f283de0c646d3fa
james-tn <james.nguyen@microsoft.com>
2022-03-15 08:11:08 -0700

    add param update to deployment

--- src/active_learning_cv/core/scoring/update_deployment.py
+++ src/active_learning_cv/core/scoring/update_deployment.py
@@ -4,6 +4,7 @@ from strictyaml import load
 from azureml.core import Workspace, Model
 from azureml.core.authentication import ServicePrincipalAuthentication
 import os
+import json
 def main(args):
     # read in data
     secret = os.environ.get("SP_SECRET")
@@ -11,17 +12,22 @@ def main(args):
     tenant_id = os.environ.get("TENANT_ID")
     subscription_id = os.environ.get("SUBSCRIPTION_ID")
     sp = ServicePrincipalAuthentication(tenant_id=tenant_id, service_principal_id=client_id,service_principal_password=secret)
-    ws = Workspace.from_config(path="src/active_learning_cv/core", auth=sp)  
+    ws = Workspace.from_config(path="src/active_learning_cv/core", auth=sp)
+    params = json.load(args.param_file)
+    model_name = params['model_name']
+    scoring_table = params['scoring_table']
+
     with open(args.job_file, 'r') as yml_file:
         yml_content = yml_file.read()
         yml_obj =load(yml_content)
     with open(args.job_file, 'w') as yml_file:  
-        current_version= Model(ws,args.model_name).version
-        yml_obj["model"] =f"azureml:{args.model_name}:{current_version}"
+        current_version= Model(ws,model_name).version
+        yml_obj["model"] =f"azureml:{model_name}:{current_version}"
         yml_obj["environment_variables"]["SP_ID"] =client_id
         yml_obj["environment_variables"]["SP_SECRET"] =secret
         yml_obj["environment_variables"]['TENANT_ID'] = tenant_id
         yml_obj["environment_variables"]['SUBSCRIPTION_ID'] = subscription_id
+        yml_obj["environment_variables"]['TABLE_NAME'] = scoring_table
         yml_file.write(yml_obj.as_yaml())
 
 
@@ -31,7 +37,7 @@ def parse_args():
 
     # add arguments
     parser.add_argument("--job_file", type=str)
-    parser.add_argument("--model_name", type=str)
+    parser.add_argument("--param_file", type=str)
 
     # parse args
     args = parser.parse_args()
