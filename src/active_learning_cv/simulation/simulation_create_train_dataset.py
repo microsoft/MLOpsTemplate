11ac137c4b5192325cd2bce54186a7fb12649c76
james-tn <james.nguyen@microsoft.com>
2022-03-14 19:53:49 -0700

    update init

--- src/active_learning_cv/simulation/simulation_create_train_dataset.py
+++ src/active_learning_cv/simulation/simulation_create_train_dataset.py
@@ -85,7 +85,7 @@ def create_aml_label_dataset(ws,datastore, target_path, input_ds, dataset_name):
     print("register  ", dataset_name)
     return dataset
 
-def create_init_train_ds(ws,datastore,train_dataset_name,jsonl_target_path, strategy, size,tenant_id,client_id,client_secret,cluster_uri,db, all_data_table_name, random_state=101):
+def create_init_train_ds(ws,datastore,train_dataset_name,jsonl_target_path, strategy,train_data_table_name, size,tenant_id,client_id,client_secret,cluster_uri,db, all_data_table_name, random_state=101):
     all_labeled_data = get_all_labeled_data(tenant_id,client_id,client_secret,cluster_uri,db, all_data_table_name)
     train_ds = all_labeled_data.sample(size,random_state=random_state)
     train_dataset, val_dataset= train_test_split(train_ds, test_size=0.2,random_state=random_state)
@@ -95,7 +95,7 @@ def create_init_train_ds(ws,datastore,train_dataset_name,jsonl_target_path, stra
     train_ds['dataset_name'] =train_aml_dataset.name
     train_ds['strategy'] =strategy
     sample_data = train_ds.head(10)
-    collector = Online_Collector(tenant_id, client_id,client_secret,cluster_uri,db,params['train_data_table_name'], sample_data)
+    collector = Online_Collector(tenant_id, client_id,client_secret,cluster_uri,db,train_data_table_name, sample_data)
     t=0
     while(t<10):
         try:
@@ -132,6 +132,7 @@ def main(args):
     train_dataset_name= params["train_dataset"]
     strategy = params['strategy']
     size = params['initial_train_size']
+    train_data_table_name=params[train_data_table_name]
     datastore = ws.datastores[datastore_name]
 
     #check if this is initial run, then create init dataset only
@@ -139,7 +140,7 @@ def main(args):
         ws.datasets[train_dataset_name] #dataset exist, then this is not the first run.
     except:
         print(f"dataset {train_dataset_name} does not exist, this is initial run, go on creating train dataset ")
-        create_init_train_ds(ws,datastore,train_dataset_name,jsonl_target_path, strategy,size,tenant_id,client_id,client_secret,cluster_uri,database_name, all_data_table_name, random_state=101)
+        create_init_train_ds(ws,datastore,train_dataset_name,jsonl_target_path, strategy,train_data_table_name,size,tenant_id,client_id,client_secret,cluster_uri,database_name, all_data_table_name, random_state=101)
         return
 
     client_secret = kv.get_secret(client_id)
