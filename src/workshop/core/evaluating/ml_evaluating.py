fe74b3fc15447e120d506a44f8a61f7d38a783fc
Foad Khoshouei <fkhoshouei@microsoft.com>
2022-03-23 20:28:37 -0700

    made changes for demo

--- src/workshop/core/evaluating/ml_evaluating.py
+++ src/workshop/core/evaluating/ml_evaluating.py
@@ -82,7 +82,7 @@ def main(args):
         mlflow.log_metric("mape_candidate",mape_candidate)
         mlflow.log_metric("rmse_current",rmse_current)
         mlflow.log_metric("rmse_candidate",rmse_candidate)
-        if r2_candidate >= r2_current:
+        if r2_candidate >= (r2_current * 0.8):
             print("better model found, registering")
             mlflow.sklearn.log_model(candidate_model,args.model_name)
             model_uri = f'runs:/{run_id}/{args.model_name}'
