f75035cfbbfe1e21e7b7087819cf402089babb35
james-tn <james.nguyen@microsoft.com>
2022-04-28 09:25:13 -0700

    fix bugs in drift analysis module & add util methods

--- src/utilities/monitoring/drift_analysis.py
+++ src/utilities/monitoring/drift_analysis.py
@@ -4,7 +4,7 @@ from monitoring import KV_SP_ID, KV_SP_KEY, KV_ADX_DB, KV_ADX_URI, KV_TENANT_ID
 
 
 class Drift_Analysis():
-    def __init__(self,table_name,ws=None,tenant_id=None, client_id=None,client_secret=None,cluster_uri=None,database_name=None):
+    def __init__(self,ws=None,tenant_id=None, client_id=None,client_secret=None,cluster_uri=None,database_name=None):
 
         if ws is not None:
             kv = ws.get_default_keyvault()
@@ -23,9 +23,25 @@ class Drift_Analysis():
         KCSB_DATA = KustoConnectionStringBuilder.with_aad_application_key_authentication(self.cluster_uri, self.client_id, self.client_secret, self.tenant_id)
         self.client = KustoClient(KCSB_DATA)
     def query(self, query):#generic query
-        response = self.client.execute(self.db, query)
+        response = self.client.execute(self.database_name, query)
         dataframe = dataframe_from_result_table(response.primary_results[0])
         return dataframe
+    def list_tables(self):
+        return list(self.query(".show tables")['TableName'])
+    def list_table_columns(self, table_name):
+        return self.query(f".show table {table_name}")[['AttributeName', 'AttributeType']]
+    def get_time_range(self, table_name, timestamp_col=None):
+        columns = self.list_table_columns(table_name)
+        timestamp_cols = columns[columns['AttributeType']=='DateTime']
+        if timestamp_cols.shape[0]==0:
+            raise Exception("No timestamp column found! ")
+        if timestamp_col is None:
+            if "timestamp" in timestamp_cols['AttributeName'].values:
+                timestamp_col ='timestamp'
+            else:
+                timestamp_col = timestamp_cols['AttributeName'].values[0]
+        time_range = self.query(f"{table_name} | summarize time_start = min(['{timestamp_col}']), time_end = max(['{timestamp_col}'])")
+        return str(time_range['time_start'].values[0]), str(time_range['time_end'].values[0])
     def analyze_drift(self, table_name, dt_from, dt_to, bin):
         pass
     def compare_drift(self, baseline_table,baseline_filter_expr, target_table, target_filter_expr):
