55a6229167d55193f4687f1ba44d1e99fd131558
Nicole Serafino <74378487+nicoleserafino@users.noreply.github.com>
2022-05-11 20:19:55 -0500

    add dashboard

--- src/utilities/monitoring/drift_analysis.py
+++ src/utilities/monitoring/drift_analysis.py
@@ -4,10 +4,19 @@ from monitoring import KV_SP_ID, KV_SP_KEY, KV_ADX_DB, KV_ADX_URI, KV_TENANT_ID
 import concurrent.futures
 from datetime import timedelta
 import pandas as pd
-from azure.ml import MLClient
-from azure.ml import command, Input
-from azure.identity import DefaultAzureCredential
-from azure.ml.entities import Environment,  BuildContext
+from jupyter_dash import JupyterDash
+import numpy as np
+import dash
+from datetime import date
+from dateutil import parser
+import plotly.express as px
+from dash import Dash, dcc, html, Input, Output, State
+import plotly.graph_objects as go
+import json
+# from azure.ml import MLClient
+# from azure.ml import command, Input
+# from azure.identity import DefaultAzureCredential
+# from azure.ml.entities import Environment,  BuildContext
 
 
 
@@ -91,8 +100,8 @@ class Drift_Analysis():
             numberical_output = self.analyze_drift_numerical(numerical_columns, time_stamp_col, base_table_name,target_table_name, base_dt_from, base_dt_to, target_dt_from, target_dt_to, bin, limit)
 
 
-
-        output =numberical_output.merge(categorical_output, how="outer", on = "target_start_date")
+        output = pd.concat([categorical_output, numberical_output])
+        # output =numberical_output.merge(categorical_output, how="outer", on = ["target_start_date"])
         return output
     def analyze_drift_v2(self,base_table_name,target_table_name, base_dt_from, base_dt_to, target_dt_from, target_dt_to, bin, limit=10000000, concurrent_run=True):
         base_tbl_columns = self.list_table_columns(base_table_name)
@@ -182,6 +191,7 @@ for i in range(n):
         target_features = random.sample(target_features, len(base_features))
     elif len(target_features) < len(base_features):
         base_features = random.sample(base_features, len(target_features))
+
     distance2.append(distance.euclidean(base_features, target_features))
 result['euclidean'] =distance2
 ```
@@ -337,35 +347,332 @@ tbl|summarize count = count() by bin({numerical_column},bin_size_temp), bin(['{t
 
         output =pd.concat([numberical_output, categorical_output])
         return output
+    def launch_dashboard(self):
+        external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-def execute_drift_detect_job(subscription_id=None,resource_group=None,workspace=None ):
+        app = JupyterDash(__name__, external_stylesheets=external_stylesheets)
 
-    subscription_id = "0e9bace8-7a81-4922-83b5-d995ff706507"
-    resource_group = "azureml"
-    workspace = "ws01ent"
-# get a handle to the workspace
-    ml_client = MLClient(
-        DefaultAzureCredential(), subscription_id, resource_group, workspace
-    )
-    env_docker_conda = Environment(
-        image="mcr.microsoft.com/azureml/openmpi3.1.2-ubuntu18.04",
-        conda_file="monitoring/drift_detection_job/conda.yml",
-        name="drift_analysis_job",
-        description="Environment created from a Docker image plus Conda environment.",
-    )
+        tables_list = self.list_tables()
 
-    job = command(
-    code="./",  # local path where the code is stored
-    command="python monitoring/drift_detection_job/job.py --base_table_name ${{inputs.base_table_name}} --target_table_name ${{inputs.target_table_name}} --base_dt_from ${{inputs.base_dt_from}} --base_dt_to ${{inputs.base_dt_to}} --target_dt_from ${{inputs.target_dt_from}} --target_dt_to ${{inputs.target_dt_to}} --bin ${{inputs.bin}} --limit ${{inputs.limit}}",
-    inputs={"base_table_name": "ISDWeather", "target_table_name": "ISDWeather", "base_dt_from":"2013-04-13", "base_dt_to": "2014-05-13","target_dt_from": "2013-04-13", "target_dt_to":"2014-05-13", "bin":"7d", "limit":10000},
-    environment=env_docker_conda,
-    compute="DS11",
-    display_name="drift-analysis-job",
-    experiment_name= "drift-analysis-job"
-    # description,
+        # 'border': '1px solid #bbb'
+        app.layout = html.Div([
+            html.Div([
+                html.Div([
+                    html.Div([
+                        html.H4(
+                            children="Base Data Selection",
+                            style={"text-align": "center"},
+                        ),
+                        html.Br(),
+                        html.Label("Select Table Name:"),
+                        dcc.Dropdown(
+                            tables_list,
+                            tables_list[0],
+                            id='tables',
+                        ),
+                        html.Br(),
+                        html.Label("Select Date Range:"),
+                        dcc.DatePickerRange(
+                            id='base_timeline',
+                            start_date_placeholder_text ="base_start_date",
+                            end_date_placeholder_text ="base_end_date"
+                        )
+                    ],
+                        style={'padding': 30, 'flex': 1}
+                    ),
+                    html.Div([
+                        html.H4(
+                            children="Target Data Selection",
+                            style={"text-align": "center"}
+                        ),
+                        html.Br(),
+                        html.Label("Select Table Name:"),
+                        dcc.Dropdown(
+                            tables_list,
+                            tables_list[0],
+                            id='trgt_tables',
+                        ),
+                        html.Br(),
+                        html.Label("Select Date Range:"),
+                        dcc.DatePickerRange(
+                            id='target_timeline',
+                            start_date_placeholder_text ="target_start_date",
+                            end_date_placeholder_text ="target_end_date"
+                        )
+                    ],
+                        style={'padding': 30, 'flex': 1}
+                    ),
+                    html.Div([
+                        html.Button('Prepare data', 
+                            id='prepare_data', 
+                            n_clicks=0,
+                            style = {'text-align': 'center', 'border': '1px solid #00ab00'}
+                        ),
+                        dcc.Store(
+                            id='intermediate-value'
+                        )
+                    ], 
+                        style={'padding': 40, 'flex': 1}
+                    )
+            ], 
+                style={'display': 'flex', 'flex-direction': 'row', 'align-items': 'flex-end', 'justify-content': 'space-between'}
+            ),
+            dcc.Tabs([
+                dcc.Tab(label="Data Overview", children=[            
+                        dcc.Graph(id='overview_graph'),
+                        dcc.Graph(id='feature_drift_graph')                       
+                ]),
+                dcc.Tab(label="Feature Drift", children=[
+                    html.Div([
+                        html.Div([
+                            html.Label("Select Feature:"),
+                            dcc.Dropdown(
+                            id='columns'
+                            )
+                        ],
+                            style={'padding': 30, 'flex': 1}
+                        ),
+                        html.Div([
+                            html.Label("Select Metric:"),
+                            dcc.Dropdown(
+                                id='metrics'
+                            ),
+                            dcc.Store(
+                                id='column-name-type'
+                            )
+                        ], 
+                            style={'padding': 30, 'flex': 1}
+                        )
+                    ],
+                    style={'display': 'flex', 'flex-direction': 'row', 'align-items': 'right'}
+                    ),
+                    html.Div([
+                        dcc.Graph(
+                            id='graph',
+                        ),
+                        dcc.Graph(
+                            id='detail_graph'
+                        )
+                    ],
+                    )
+                ])
+            ])
+            ])
+        ]
+        )
+
+        @app.callback(
+            Output('trgt_tables', 'value'),
+            Input('tables', 'value'))
+        def set_columns_options(table_name):
+            return table_name
+
+        @app.callback(
+            Output('columns', 'options'),
+            Input('tables', 'value'))
+        def set_columns_options(table_name):
+            return self.list_table_columns(table_name)['AttributeName'].values
+        @app.callback(
+            Output('column-name-type', 'data'),
+            Input('tables', 'value'))
+        def set_columns_options(table_name):
+            return self.list_table_columns(table_name).to_json(date_format='iso', orient='split')
+
+        @app.callback(
+            Output('columns', 'value'),
+            Input('columns', 'options'))
+        def set_columns_value(available_options):
+            return available_options[0]
+
+        @app.callback(
+            Output('metrics', 'options'),
+            Input('columns', 'value'),
+            Input('column-name-type', 'data'),
+        )
+        def set_metric_options(column, column_dict):
+            column_df = pd.read_json(column_dict, orient='split')
+            column_type = str(column_df[column_df['AttributeName']==column]['AttributeType'].values[0])
+            if column_type=='StringBuffer':
+                return ['dcount', 'euclidean']
+            elif column_type=='DateTime':
+                return ['NA']
+            else:
+                return ['max', 'min', "mean", "wasserstein"]
+        @app.callback(
+            Output('metrics', 'value'),
+            Input('metrics', 'options'))
+        def set_columns_value(available_options):
+            return available_options[0]
+
+
+        @app.callback(
+            [Output('base_timeline', 'min_date_allowed'),Output('base_timeline', 'max_date_allowed'),Output('base_timeline', 'initial_visible_month'),Output('base_timeline', 'start_date'), Output('base_timeline', 'end_date')],
+            Input('tables', 'value'))
+        def set_columns_options(table_name):
+            time_range =self.get_time_range(table_name)
+
+            start_date = parser.parse(time_range[0]).replace(microsecond=0, second=0, minute=0)
+            end_date = parser.parse(time_range[1]).replace(microsecond=0, second=0, minute=0)
+            
+            return [start_date, end_date,end_date, start_date, end_date]
+
+        @app.callback(
+            [Output('target_timeline', 'min_date_allowed'),Output('target_timeline', 'max_date_allowed'),Output('target_timeline', 'initial_visible_month'),Output('target_timeline', 'start_date'), Output('target_timeline', 'end_date')],
+            Input('trgt_tables', 'value'))
+        def set_columns_options(table_name):
+            time_range =self.get_time_range(table_name)
+
+            start_date = parser.parse(time_range[0]).replace(microsecond=0, second=0, minute=0)
+            end_date = parser.parse(time_range[1]).replace(microsecond=0, second=0, minute=0)
+            
+            return [start_date, end_date,end_date, start_date, end_date]
+
+        @app.callback(
+            Output('intermediate-value', 'data'),
+            State('tables', 'value'),
+            State('base_timeline', 'start_date'),
+            State('base_timeline', 'end_date'), 
+            State('trgt_tables', 'value'),
+            State('target_timeline', 'start_date'),
+            State('target_timeline', 'end_date'), 
+            Input('prepare_data', 'n_clicks'),
+                prevent_initial_call=True
+
+            
+            )
+        def calculate(table_name, base_start_date, base_end_date,target_table_name,target_start_date, target_end_date,n_clicks):
+                    # self,base_table_name,target_table_name, base_dt_from, base_dt_to, target_dt_from, target_dt_to, bin, limit=10000000, concurrent_run=True)
+            query_df = self.analyze_drift(limit=100000,base_table_name = f'{table_name}', target_table_name=f'{target_table_name}', base_dt_from=f'{base_start_date}', base_dt_to=f'{base_end_date}', target_dt_from=f'{target_start_date}', target_dt_to=f'{target_end_date}', bin='30d')
+            return query_df.to_json(date_format='iso', orient='split')
+
+        @app.callback(
+            Output('overview_graph', 'figure'),
+            Input('intermediate-value', 'data'),
+            prevent_initial_call=True
+            )
+        def update_drift_figure(jsonified_cleaned_data):
+            x_values =[0]
+            y_values =[0]
+            if jsonified_cleaned_data is not None:
+                dff = pd.read_json(jsonified_cleaned_data, orient='split')
+                dff.sort_values("target_start_date", inplace=True)
+                dff["feature_drift"] = np.where(pd.isna(dff["euclidean"]), dff["wasserstein"]/(dff["target_mean"]+1), dff["euclidean"]/(dff["target_dcount"]+1))
+                avgs = dff.groupby("target_start_date")["feature_drift"].mean()
+                # x_values = list(avgs.index.strftime("%m/%d/%Y"))
+                x_values = list(avgs.index)
+                y_values = list(avgs.values)
+
+            fig = go.Figure([go.Scatter(x=x_values, y=y_values)])
+            fig.update_layout(title_text="Data Drift", title_x=0.5)
+
+            return fig
+
+        @app.callback(
+            Output('graph', 'figure'),
+            Input('columns', 'value'),
+            Input('metrics', 'value'),
+            Input('intermediate-value', 'data'),
+            prevent_initial_call=True
+
+            )
+        def update_figure(column,metric, jsonified_cleaned_data):
+            x_values =[0]
+            y_values =[0]
+            if jsonified_cleaned_data is not None:
+                dff = pd.read_json(jsonified_cleaned_data, orient='split')
+                dff.sort_values("target_start_date", inplace=True)
+                if metric != "wasserstein" and metric != "euclidean" and metric !='NA':
+                    metric = "target_"+metric
+                if dff[dff['numeric_feature']==column].shape[0]>0:
+                    y_values = dff[dff['numeric_feature']==column][metric]
+                    x_values= dff[dff['numeric_feature']==column]["target_start_date"]
+                elif dff[dff['categorical_feature']==column].shape[0]>0:
+                    y_values = dff[dff['categorical_feature']==column][metric]
+
+                    x_values = dff[dff['categorical_feature']==column]["target_start_date"]
+
+
+            fig = go.Figure([go.Scatter(x=x_values, y=y_values, name=f"{column}")])
+            fig.update_layout(title_text="Feature Analysis", title_x=0.5)
+
+            return fig
+        @app.callback(
+            Output('detail_graph', 'figure'),
+            Input('graph', 'clickData'),   
+            Input('tables', 'value'),
+            Input('columns', 'value')
+        )
+        def drilldown(click_data,table, column):
+
+            # using callback context to check which input was fired
+            ctx = dash.callback_context
+            trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
+
+            if trigger_id == 'graph':
+
+                if click_data is not None:
+                    start_date = click_data['points'][0]['x']
+                    output = self.sample_data_points(table,column,start_date,horizon=30)
+                    output.dropna(inplace=True)
+                    fig = px.histogram(output,x=column )
+                    fig.update_layout(title_text="Feature Distribution", title_x=0.5)
+                    return fig
+
+            else:
+                fig = go.Figure(data=[go.Histogram(x=[0] )])
+                fig.update_layout(title_text="Feature Distribution", title_x=0.5)
+                return fig
+
+        @app.callback(
+            Output('feature_drift_graph', 'figure'),
+            Input('intermediate-value', 'data'),
+            prevent_initial_call=True
+            )
+        def update_feat_figure(jsonified_cleaned_data):
+
+            if jsonified_cleaned_data is not None:
+                dff = pd.read_json(jsonified_cleaned_data, orient='split')
+                dff.sort_values("target_start_date", inplace=True)
+                dff["feature_drift"] = np.where(pd.isna(dff["euclidean"]), dff["wasserstein"]/(dff["target_mean"]+1), dff["euclidean"]/(dff["target_dcount"]+1))
+                dff["feature_name"] = np.where(pd.isna(dff["euclidean"]), dff["numeric_feature"], dff["categorical_feature"])
+                fig = px.bar(dff, x="target_start_date", y="feature_drift", color="feature_name")    
+            else:
+                fig = go.Figure(data=[go.Histogram(x=[0] )])
+            fig.update_layout(title_text="Data Drift by Feature", title_x=0.5)
+            return fig
+
+
+
+        app.run_server()
+
+# def execute_drift_detect_job(subscription_id=None,resource_group=None,workspace=None ):
+
+#     subscription_id = "0e9bace8-7a81-4922-83b5-d995ff706507"
+#     resource_group = "azureml"
+#     workspace = "ws01ent"
+# # get a handle to the workspace
+#     ml_client = MLClient(
+#         DefaultAzureCredential(), subscription_id, resource_group, workspace
+#     )
+#     env_docker_conda = Environment(
+#         image="mcr.microsoft.com/azureml/openmpi3.1.2-ubuntu18.04",
+#         conda_file="monitoring/drift_detection_job/conda.yml",
+#         name="drift_analysis_job",
+#         description="Environment created from a Docker image plus Conda environment.",
+#     )
+
+#     job = command(
+#     code="./",  # local path where the code is stored
+#     command="python monitoring/drift_detection_job/job.py --base_table_name ${{inputs.base_table_name}} --target_table_name ${{inputs.target_table_name}} --base_dt_from ${{inputs.base_dt_from}} --base_dt_to ${{inputs.base_dt_to}} --target_dt_from ${{inputs.target_dt_from}} --target_dt_to ${{inputs.target_dt_to}} --bin ${{inputs.bin}} --limit ${{inputs.limit}}",
+#     inputs={"base_table_name": "ISDWeather", "target_table_name": "ISDWeather", "base_dt_from":"2013-04-13", "base_dt_to": "2014-05-13","target_dt_from": "2013-04-13", "target_dt_to":"2014-05-13", "bin":"7d", "limit":10000},
+#     environment=env_docker_conda,
+#     compute="DS11",
+#     display_name="drift-analysis-job",
+#     experiment_name= "drift-analysis-job"
+#     # description,
     
-)
+# )
 
-    returned_job = ml_client.create_or_update(job)
+#     returned_job = ml_client.create_or_update(job)
 
 
