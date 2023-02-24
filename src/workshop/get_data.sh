1a0124bd6037aa0f2515a75617717372160107da
John Bellamy <johb3406@gmail.com>
2023-02-24 17:52:24 +0000

    Update

---
+++ src/workshop/get_data.sh
@@ -0,0 +1,1 @@
+python ./data/create_datasets.py --datastore_name workspaceblobstore --ml_workspace_name "aml870529" --sub_id "db6b8621-51b0-4417-a069-65f2eeb10206" --resourcegroup_name "mlops-rg-870529"
